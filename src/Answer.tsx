59e6d13f0f25fdf6a20e41260745844bf34ad918
seunwon <angelaseo2290@gmail.com>
2024-08-19 03:36:02 +0900

    makes logic

--- src/Answer.tsx
+++ src/Answer.tsx
@@ -2,6 +2,9 @@ export function GenerateNumbers(): [number, number, number] {
   let first = Math.floor(Math.random() * 10);
   let second = Math.floor(Math.random() * 10);
   let third = Math.floor(Math.random() * 10);
+  while (first === 0) {
+    first = Math.floor(Math.random() * 10);
+  }
 
   while (second === first) {
     second = Math.floor(Math.random() * 10);
--- src/Game.tsx
+++ src/Game.tsx
@@ -1,19 +1,51 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { GenerateNumbers } from "./Answer";
 
 function StartPage() {
   const [count, setCount] = useState(0);
   const [guess, setGuess] = useState("");
   const [finalGuess, setfinalGuess] = useState("");
+  const [answer, setAnswer] = useState<[number, number, number] | null>(null);
+  const [ball, setBall] = useState(0);
+  const [strike, setStrike] = useState(0);
+
+  useEffect(() => {
+    const x = GenerateNumbers();
+    setAnswer(x);
+    console.log(x);
+  }, []);
 
   function handleChange(e) {
     setGuess(e.target.value);
-    console.log(e.target.value);
   }
 
   function addGuess() {
     setCount(count + 1);
-    setfinalGuess(guess);
-    console.log(finalGuess);
+    setfinalGuess(finalGuess);
+    hitNumber(guess);
+  }
+
+  function hitNumber(currentGuess) {
+    if (!answer) return;
+
+    const numbers = currentGuess.split("").map(Number);
+    let s = 0;
+    let b = 0;
+    for (let i = 0; i < 3; i++) {
+      if (numbers[i] === answer[i]) {
+        s++;
+      }
+    }
+
+    for (let i = 0; i < 3; i++) {
+      if (answer.includes(numbers[i]) && numbers[i] != answer[i]) {
+        b++;
+      }
+    }
+    setBall(b);
+    setStrike(s);
+    console.log(b);
+    console.log(s);
   }
 
   return (
@@ -21,11 +53,14 @@ function StartPage() {
       <div className="App">
         <input
           type="text"
+          value={guess}
           onChange={handleChange}
           placeholder="숫자 세개를 입력하세요"
-        ></input>
+        />
         <button onClick={addGuess}>submit</button>
-        <p>You typed: {finalGuess}</p>
+        <p>You typed: {guess}</p>
+        <p>Strikes: {strike}</p>
+        <p>Balls: {ball}</p>
       </div>
     </>
   );
