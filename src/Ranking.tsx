4972fc423c49d7662802132f0a3ebb7e2360d3b6
seunwon <angelaseo2290@gmail.com>
2024-09-13 23:07:26 +0900

    점수 등록하기

--- src/Ranking.tsx
+++ src/Ranking.tsx
@@ -18,12 +18,23 @@ function RankingPage() {
     navigate("/");
   }
 
+  let userRanking = [{ name: "은원", score: "3" }];
+
+  function addArray() {
+    userRanking.push({ name: player, score: score });
+    console.log(userRanking);
+  }
+
   return (
     <>
       <h2>점수</h2>
       <h3 id="pixel">
         {player}, {score}
       </h3>
+      <button id="pixel" onClick={addArray}>
+        점수 등록하기
+      </button>
+      <p></p>
       <button id="pixel" onClick={handleFirstPage}>
         처음으로
       </button>
